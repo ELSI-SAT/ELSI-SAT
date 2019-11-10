8e20bac00cb30e11e4bba69f42fe5c1791cb5839
Uwe Stange <uwe.stange@gmail.com>
2019-11-10 15:22:07 +0100

    Modify footer

--- src/layouts/components/TheFooter.vue
+++ src/layouts/components/TheFooter.vue
@@ -11,11 +11,7 @@
 
 <template functional>
     <footer class="the-footer flex-wrap justify-between" :class="classes">
-        <span>COPYRIGHT @ {{ new Date().getFullYear() }} <a href="https://1.envato.market/vuexy_admin" target="_blank" rel="nofollow">Pixinvent</a>, All rights Reserved</span>
-        <span class="md:flex hidden items-center">
-            <span>Hand-crafted & Made with</span>
-            <feather-icon icon="HeartIcon" svgClasses="stroke-current text-danger w-6 h-6" class="ml-2" />
-        </span>
+        <span>ELSI-SAT <a href="#">Datenschutz</a> • <a href="#">Impressum</a></span>
     </footer>
 </template>
 
