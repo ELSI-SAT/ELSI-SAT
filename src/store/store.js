4be474583fa7d49ceb23cda0c522dbd949153e86
Uwe Stange <uwe.stange@gmail.com>
2019-11-11 15:22:40 +0100

    Remove superfluous imports from store.js

--- src/store/store.js
+++ src/store/store.js
@@ -18,13 +18,8 @@ import actions from "./actions"
 
 Vue.use(Vuex)
 
-// import moduleTodo from './todo/moduleTodo.js'
-// import moduleCalendar from './calendar/moduleCalendar.js'
-// import moduleChat from './chat/moduleChat.js'
+// Todo-usta: atm loaded in Email.vue
 // import moduleEmail from './email/moduleEmail.js'
-import moduleAuth from './auth/moduleAuth.js'
-import moduleECommerce from './eCommerce/moduleECommerce.js'
-
 
 export default new Vuex.Store({
     getters,
@@ -32,12 +27,7 @@ export default new Vuex.Store({
     state,
     actions,
     modules: {
-        // todo: moduleTodo,
-        // calendar: moduleCalendar,
-        // chat: moduleChat,
         // email: moduleEmail,
-        auth: moduleAuth,
-        eCommerce: moduleECommerce,
     },
     strict: process.env.NODE_ENV !== 'production'
 })
