e4bc84cd6c1a636a056a8ea6097cdc3f5cb980de
Uwe Stange <uwe.stange@gmail.com>
2019-11-11 09:31:16 +0100

    Delete fake-db-2 backup directory

--- src/fake-db 2/data/apps/index.js
+++
@@ -1,2 +0,0 @@
-import "./todo/index.js"
-import "./email/index.js"
--- src/fake-db 2/index.js
+++
@@ -1,7 +0,0 @@
-import mock from './mock'
-
-
-// Apps
-import "./data/apps/index.js"
-
-mock.onAny().passThrough(); // forwards the matched request over network
