2342c421a889ee46f6971d90e9d9590c4433a95c
Uwe Stange <uwe.stange@gmail.com>
2019-11-23 12:11:26 +0100

    Test: use mapGetters to access store

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,16 @@
 <template>
 	<h4>You are in home.</h4>
 </template>
+
+<script>
+  import { mapGetters } from "vuex";
+
+  export default {
+    name: "Home",
+    computed: mapGetters(['email']),
+    created() {
+      console.log('Email.vue created')
+      console.log(this.$store.state.email)
+    }
+  }
+</script>
