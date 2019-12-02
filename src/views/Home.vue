9ec6299cee1dcf36af8c2cf732eae99d4d5d78e1
Uwe Stange <uwe.stange@gmail.com>
2019-12-02 17:19:07 +0100

    Make static radialbar-chart dynamic

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,44 +1,24 @@
 <template>
   <div>
-    <h4>You are in home.</h4>
-
     <div class="vx-row">
       <div class="vx-col w-1/2 mb-base">
         <vx-card
-          title="Radialbar"
+          title="Beantwortete Fragen"
           title-color="primary"
           subtitle="">
-          <p>Lorem ipsum.</p>
           <vue-apex-charts type="radialBar"
                            height="350"
                            :options="radialBarChart.chartOptions"
-                           :series="radialBarChart.series">
+                           :series="quota">
           </vue-apex-charts>
         </vx-card>
       </div>
 
-      <div class="vx-col w-1/2 mb-base">
-        <vx-card
-          title="Heatmap"
-          title-color="primary"
-          subtitle="">
-          <p>Lorem ipsum.</p>
-          <vue-apex-charts type="heatmap"
-                           height="316"
-                           :options="heatMapChart.chartOptions"
-                           :series="heatMapChart.series">
-          </vue-apex-charts>
-        </vx-card>
-      </div>
-    </div>
-
-    <div class="vx-row">
       <div class="vx-col w-1/2 mb-base">
         <vx-card
           title="Bar"
           title-color="primary"
           subtitle="">
-          <p>Lorem ipsum.</p>
           <vue-apex-charts type="bar"
                            height="200"
                            :options="barChart.chartOptions"
@@ -46,6 +26,23 @@
           </vue-apex-charts>
         </vx-card>
       </div>
+
+    </div>
+
+    <div class="vx-row">
+      <div class="vx-col w-full mb-base">
+        <vx-card
+          title="Heatmap"
+          title-color="primary"
+          subtitle="">
+          <vue-apex-charts type="heatmap"
+                           height="316"
+                           :options="heatMapChart.chartOptions"
+                           :series="heatMapChart.series">
+          </vue-apex-charts>
+        </vx-card>
+      </div>
+
 
     </div>
 
@@ -59,14 +56,23 @@
   export default {
     name: "Home",
 
-    computed: mapGetters(['email']),
+    computed: {
+      // mix the getters into computed with object spread operator
+      ...mapGetters([
+        'email',
+      ]),
 
-    created() {
-      console.log('Email.vue created')
-      console.log(this.$store.state.email)
+      quota () {
+        const answered = this.$store.getters['email/getNumberOfAnswers']
+        const questions = this.$store.getters['email/getNumberOfQuestions']
+        const quota = Math.floor((answered / questions) * 100)
+        // Return an array!
+        return [quota]
+      },
     },
 
     methods: {
+
       generateData(count, yrange) {
         let i = 0;
         let series = [];
@@ -88,31 +94,56 @@
       return {
 
         radialBarChart: {
-          series: [44, 55, 67, 83],
+          series: ["0"],
           chartOptions: {
-            colors: ['#7367F0', '#28C76F', '#EA5455', '#FF9F43', '#1E1E1E'],
             plotOptions: {
               radialBar: {
+                size: 140,
+                offsetY: -30,
+                startAngle: -150,
+                endAngle: 150,
+                hollow: {
+                  size: '60%',
+                },
+                track: {
+                  background: "#f8f8f8",
+                  strokeWidth: '100%',
+
+                },
                 dataLabels: {
-                  name: {
-                    fontSize: '22px',
-                  },
                   value: {
-                    fontSize: '16px',
-                  },
-                  total: {
-                    show: true,
-                    label: 'Total',
-                    formatter: function (w) {
-                      // By default this function returns the average of all series. The below is just an example to show the use of custom formatter function
-                      return 249
-                    }
+                    offsetY: 0,
+                    color: '#99a2ac',
+                    fontSize: '2rem'
                   }
                 }
-              }
+              },
             },
-            labels: ['Apples', 'Oranges', 'Bananas', 'Berries'],
-          }
+            colors: ['#7367F0'],
+            fill: {
+              // type: 'gradient',
+              // gradient: {
+              //   // enabled: true,
+              //   shade: 'dark',
+              //   type: 'horizontal',
+              //   shadeIntensity: 0.5,
+              //   gradientToColors: ['#76B9F4'],
+              //   inverseColors: true,
+              //   opacityFrom: 1,
+              //   opacityTo: 1,
+              //   stops: [0, 100]
+              // },
+            },
+            stroke: {
+              // dashArray: 8
+            },
+            chart: {
+              toolbar: {
+                show: true,
+              },
+            },
+            labels: [''],
+          },
         },
 
 
