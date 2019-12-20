ELSI-SAT/ELSI-SAT

commit 4de936e15690f400304c62047ade71fd4fbfcce8

Create type "filter"

src/views/apps/email/EmailMailCard.vue
@@ -9,20 +9,34 @@
 
 <template>
   <vx-card class="px-4">
-    <div class="vx-row">
+    <!-- Filter Questions -->
+    <div
+      class="vx-row"
+      v-if="mailContent.filter"
+    >
+      <FilterForm
+        :mailContent="mailContent"
+        class="w-full"></FilterForm>
+    </div>
+
+    <!-- Regular Questions -->
+    <div
+      class="vx-row"
+      v-if="!mailContent.isTrashed && !mailContent.filter"
+    >
       <TextForm
-        v-if="!mailContent.isTrashed && mailContent.answer.type.includes('text')"
+        v-if="mailContent.answer.type.includes('text')"
         :mailContent="mailContent"
         :textsize="mailContent.answer.type"
         class="w-full"></TextForm>
 
       <RadioForm
-        v-if="!mailContent.isTrashed && mailContent.answer.type === 'radio'"
+        v-if="mailContent.answer.type === 'radio'"
         :mailContent="mailContent"
         class="w-full mb-4"></RadioForm>
 
       <CheckboxForm
-        v-if="!mailContent.isTrashed && mailContent.answer.type === 'checkbox'"
+        v-if="mailContent.answer.type === 'checkbox'"
         :mailContent="mailContent"
         class="w-full mb-4"></CheckboxForm>
 
@@ -46,6 +60,8 @@
 </template>
 
 <script>
+  import FilterForm from './forms/FilterForm.vue'
+
   import TextForm from './forms/TextForm.vue'
   import RadioForm from './forms/RadioForm.vue'
   import CheckboxForm from './forms/CheckboxForm.vue'
@@ -55,6 +71,8 @@
     name: 'EMailMailCard',
 
     components: {
+      FilterForm,
+
       TextForm,
       RadioForm,
       CheckboxForm,

src/store/email/moduleEmailActions.js
@@ -33,6 +33,11 @@ export default {
     dispatch('fetchMeta')
   },
 
+  // Commit filter updates
+  updateFilterQuestion({ commit }, payload) {
+    commit("UPDATE_FILTER_QUESTION", payload)
+  },
+
   // Fetch Email Meta
   fetchMeta({ commit, state }) {
 

src/store/email/moduleEmailMutations.js
@@ -93,4 +93,8 @@ export default {
     UPDATE_TRASHREASON(state, payload) {
       state.mails.find((mail) => mail.id === payload.id).trashingReason = payload.answer
     },
+
+    UPDATE_FILTER_QUESTION(state, payload) {
+      state.mails.find((mail) => mail.id === payload.id).filter = payload.filters
+    },
 }

src/store/email/moduleEmailState.js
@@ -122,7 +122,7 @@ export default {
     },
     {
       id: 12,
-      inquiry: 'Welche Maßnahmen ergreifen Sie, um xyz zu verhindern?',
+      inquiry: 'Datenschutz oder Allgemein & K.I. (filter)?',
       subject: 'Frage 12',
       message: '<p>Lorem ipsum dolor sit amet, <i>consetetur sadipscing elitr</i>, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </p>',
       isStarred: false, // true || false
@@ -132,6 +132,19 @@ export default {
       folder: 'inbox', // answered || draft || inbox || trash
       isTrashed: false, // true || false
       trashingReason: '', // String
+      // Filter: Optional array of objects
+      filter: [
+        {
+          title: 'Allgemein und K.I.',
+          includes: ['Allgemein', 'K.I.'],
+          active: false
+        },
+        {
+          title: 'Datenschutz',
+          includes: ['Datenschutz'],
+          active: false
+        },
+      ],
       answer: {
         type: "text", // String: 'tinytext' (100) | 'text' (500) | 'bigtext' (2000) | 'radio' (mutually exclusive) | 'checkbox' (multiple)
         options: [], // Array of options for types 'radio' and 'multiple'
@@ -540,12 +553,15 @@ export default {
     },
   ],
 
+  // Tags and labels
   mailTags: [
     {text: 'Allgemein', value: 'Allgemein', color: "#28C770"},
     {text: 'K.I.', value: 'K.I.', color: "#7367F1"},
     {text: 'Datenschutz', value: 'Datenschutz', color: "#FFA044"},
     {text: 'Gesetzliche Anforderungen', value: 'Gesetzliche Anforderungen', color: "#EA5556"},
   ],
+
+  // Sidebar-filter for state (inbox, answered) and labels.
   mail_filter: null,
   meta: {},
   mailSearchQuery: '',

src/views/apps/email/forms/FilterForm.vue
@@ -1,0 +1,93 @@
+<template>
+  <ul class="centerx">
+    <li v-for="(item, index) in  mailContent.filter" v-bind:key="index">
+      <vs-checkbox v-model="checkbox" :vs-value="item">{{ item.title }}</vs-checkbox>
+    </li>
+  </ul>
+</template>
+
+<script>
+  import {mapGetters} from "vuex";
+
+  export default {
+    name: "FilterForm",
+
+    props: {
+      mailContent: {
+        type: Object,
+        required: true
+      },
+    },
+
+    computed: {
+      ...mapGetters([]),
+
+      checkbox: {
+        get() {
+          let activeFilters = []
+          const filters = this.$store.getters['email/getMail'](this.mailContent.id).filter
+
+          filters.forEach(function (filter) {
+            filter.active ? activeFilters.push(filter) : ''
+          });
+
+          return activeFilters
+        },
+
+        set(value) {
+          const activeFilters = value
+          let numberOfActiveFilters = 0
+
+          // Make a real copy of a JavaScript Array with Objects (without a reference)!
+          // @see discussion of [...array] and other methods (that only create shallow copies) at
+          // https://dev.to/rsschouwenaar/how-to-make-a-real-copy-of-a-javascript-array-with-objects-without-a-reference-5md
+          // Alternatively use lodash's https://lodash.com/docs/4.17.15#cloneDeep
+          let filters =
+            JSON.parse(
+              JSON.stringify(
+                this.$store.getters['email/getMail'](this.mailContent.id).filter
+              )
+            );
+
+          // Set selected filters to true
+          filters.forEach(function (filter) {
+            // Reset all filters to false …
+            filter.active = false
+
+            activeFilters.forEach(function (activeFilter) {
+              // … except those that are set in the form.
+              if (filter.title == activeFilter.title) {
+                filter.active = true
+                numberOfActiveFilters++
+              }
+            })
+          })
+
+          const payload = {
+            id: this.mailContent.id,
+            filters: filters,
+          }
+
+          // Dispatch the filters-array.
+          this.$store.dispatch('email/updateFilterQuestion', payload)
+
+          // Set question as answered
+          if (numberOfActiveFilters > 0) {
+            this.$store.commit('email/UPDATE_ANSWER', {answer: 'filterIsSet', id: this.mailContent.id})
+          } else {
+            this.$store.commit('email/UPDATE_ANSWER', {answer: '', id: this.mailContent.id})
+          }
+
+          // Todo usta: Maybe instead of dispatching this action
+          // that always scans the complete state
+          // instead create a button that submits the form once.
+          this.$store.dispatch("email/fetchMeta");
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>

commit 8d36d258956dd1d6bb1f63aab94af566b0b685c3

Hide trash-button from followup-children
- also put `<TrashReason />` in a new row so it will be visible when a question is trashed (bug was introduced in 4de936e1)

src/views/apps/email/EmailMailCard.vue
@@ -12,8 +12,7 @@
     <!-- Filter Questions -->
     <div
       class="vx-row"
-      v-if="mailContent.filter"
-    >
+      v-if="mailContent.filter">
       <FilterForm
         :mailContent="mailContent"
         class="w-full"></FilterForm>
@@ -22,8 +21,8 @@
     <!-- Regular Questions -->
     <div
       class="vx-row"
-      v-if="!mailContent.isTrashed && !mailContent.filter"
-    >
+      v-if="!mailContent.isTrashed && !mailContent.filter">
+
       <TextForm
         v-if="mailContent.answer.type.includes('text')"
         :mailContent="mailContent"
@@ -44,9 +43,12 @@
         v-if="mailContent.answer.type === 'followup'"
         :mailContent="mailContent"
         class="w-full mb-4"></FollowupForm>
+    </div>
 
+    <div
+      class="vx-row">
       <vs-button
-        v-if="!isFollowupChild"
+        v-if="!isFollowupChild && !mailContent.filter"
         @click="toggleIsTrashed"
         color="danger"
         :type="mailContent.isTrashed ? 'filled' : 'border'"
@@ -60,7 +62,6 @@
         :mailContent="mailContent"
         label="Eingaben werden sofort gespeichert."
         class="w-full mt-4"/>
-
     </div>
   </vx-card>
 </template>