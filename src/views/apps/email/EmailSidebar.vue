7d564641a6293586fe8f0dbb5ad6618f844ad2bd
Stange Uwe <stange@hdm-stuttgart.de>
2019-12-28 15:15:35 +0100

    Refactor EmailSidebar.vue

--- src/views/apps/email/EmailSidebar.vue
+++ src/views/apps/email/EmailSidebar.vue
@@ -10,65 +10,108 @@
 
 <template>
     <div class="email__email-sidebar h-full">
-        <div class="m-6 clearfix">
-        </div>
+        <div class="m-6 clearfix"></div>
 
-        <VuePerfectScrollbar class="email-scroll-area" :settings="settings">
+        <VuePerfectScrollbar
+          class="email-scroll-area"
+          :settings="settings">
             <div class="px-6 pb-2 flex flex-col">
 
-                <!-- inbox -->
-                <router-link tag="span" :to="`${baseUrl}/inbox`" class="flex justify-between items-center cursor-pointer" :class="{'text-primary': mailFilter == 'inbox'}">
-                    <div class="flex items-center mb-2">
-                        <feather-icon icon="CircleIcon" :svgClasses="[{'text-primary stroke-current': mailFilter == 'inbox'}, 'h-6 w-6']"></feather-icon>
-                        <span class="text-lg ml-3">Unbeantwortet</span>
-                    </div>
-                    <template>
-                      <vs-chip class="number pr-2 pl-2" color="primary">{{ mailMeta.inbox }}</vs-chip>
-                    </template>
-                </router-link>
+              <!-- inbox -->
+              <router-link
+                tag="span"
+                :to="`${baseUrl}/inbox`"
+                class="flex justify-between items-center cursor-pointer"
+                :class="{'text-primary': mailFilter == 'inbox'}">
 
-                <!-- answered -->
-                <router-link tag="span" :to="`${baseUrl}/answered`" class="flex justify-between items-center cursor-pointer mt-4" :class="{'text-primary': mailFilter == 'answered'}">
+                <div class="flex items-center mb-2">
+                    <feather-icon icon="CircleIcon" :svgClasses="[{'text-primary stroke-current': mailFilter == 'inbox'}, 'h-6 w-6']"></feather-icon>
+                    <span class="text-lg ml-3">Unbeantwortet</span>
+                </div>
+
+                <template>
+                  <vs-chip class="number pr-2 pl-2" color="primary">{{ mailMeta.inbox }}</vs-chip>
+                </template>
+
+              </router-link>
+
+              <!-- answered -->
+              <router-link
+                tag="span"
+                :to="`${baseUrl}/answered`"
+                class="flex justify-between items-center cursor-pointer mt-4"
+                :class="{'text-primary': mailFilter == 'answered'}">
+
+                <div class="flex items-center mb-2">
+                  <feather-icon icon="CheckCircleIcon" :svgClasses="[{'text-primary stroke-current': mailFilter == 'answered'}, 'h-6 w-6']"></feather-icon>
+                  <span class="text-lg ml-3">Beantwortet</span>
+                </div>
+
+                <template>
+                  <vs-chip class="number pr-2 pl-2" color="success">{{ mailMeta.answered }}</vs-chip>
+                </template>
+
+              </router-link>
+
+              <!-- starred -->
+              <router-link
+                tag="span"
+                :to="`${baseUrl}/starred`"
+                class="flex justify-between items-center cursor-pointer mt-4"
+                :class="{'text-primary': mailFilter == 'starred'}">
+
+                <div class="flex items-center mb-2">
+                  <feather-icon icon="StarIcon" :svgClasses="[{'text-primary stroke-current': mailFilter == 'starred'}, 'h-6 w-6']"></feather-icon>
+                  <span class="text-lg ml-3">Markiert</span>
+                </div>
+
+                <template>
+                  <vs-chip class="number pr-2 pl-2" color="warning">{{ mailMeta.starred }}</vs-chip>
+                </template>
+
+              </router-link>
+
+              <!-- trash -->
+              <router-link
+                tag="span"
+                :to="`${baseUrl}/trash`"
+                class="flex justify-between items-center cursor-pointer mt-4"
+                :class="{'text-primary': mailFilter == 'trash'}">
+
                   <div class="flex items-center mb-2">
-                    <feather-icon icon="CheckCircleIcon" :svgClasses="[{'text-primary stroke-current': mailFilter == 'answered'}, 'h-6 w-6']"></feather-icon>
-                    <span class="text-lg ml-3">Beantwortet</span>
+                    <feather-icon icon="TrashIcon" :svgClasses="[{'text-primary stroke-current': mailFilter == 'trash'}, 'h-6 w-6']"></feather-icon>
+                    <span class="text-lg ml-3">Nicht relevant</span>
                   </div>
-                  <template>
-                    <vs-chip class="number pr-2 pl-2" color="success">{{ mailMeta.answered }}</vs-chip>
-                  </template>
-                </router-link>
 
-                <!-- starred -->
-                <router-link tag="span" :to="`${baseUrl}/starred`" class="flex justify-between items-center cursor-pointer mt-4" :class="{'text-primary': mailFilter == 'starred'}">
-                    <div class="flex items-center mb-2">
-                      <feather-icon icon="StarIcon" :svgClasses="[{'text-primary stroke-current': mailFilter == 'starred'}, 'h-6 w-6']"></feather-icon>
-                      <span class="text-lg ml-3">Markiert</span>
-                    </div>
-                  <template>
-                    <vs-chip class="number pr-2 pl-2" color="warning">{{ mailMeta.starred }}</vs-chip>
-                  </template>
-                </router-link>
-
-                <!-- trash -->
-                <router-link tag="span" :to="`${baseUrl}/trash`" class="flex justify-between items-center cursor-pointer mt-4" :class="{'text-primary': mailFilter == 'trash'}">
-                    <div class="flex items-center mb-2">
-                      <feather-icon icon="TrashIcon" :svgClasses="[{'text-primary stroke-current': mailFilter == 'trash'}, 'h-6 w-6']"></feather-icon>
-                      <span class="text-lg ml-3">Nicht relevant</span>
-                    </div>
                   <template>
                     <vs-chip class="number pr-2 pl-2" color="danger">{{ mailMeta.trashed }}</vs-chip>
                   </template>
-                </router-link>
+              </router-link>
+
             </div>
+
             <vs-divider></vs-divider>
+
             <div class="email__labels px-6 py-4">
-                <h5 class="mb-8">Kategorien</h5>
-                <div class="email__lables-list">
-                    <router-link tag="span" class="email__label flex items-center mb-4 cursor-pointer" v-for="(tag, index) in mailTags" :key="index" :to="`${baseUrl}/${tag.value}`">
-                        <div class="ml-1 h-3 w-3 rounded-full mr-4" :class="'border-2 border-solid'" :style="'border-color: ' + tag.color"></div>
-                        <span class="text-lg" :class="{'text-primary': mailFilter == tag.value}">{{ tag.text }}</span>
-                    </router-link>
-                </div>
+              <h5 class="mb-8">Kategorien</h5>
+              <div class="email__lables-list">
+                <router-link
+                  tag="span"
+                  class="email__label flex items-center mb-4 cursor-pointer"
+                  v-for="(tag, index) in mailTags"
+                  :key="index"
+                  :to="`${baseUrl}/${tag.value}`">
+
+                  <div
+                    class="ml-1 h-3 w-3 rounded-full mr-4"
+                    :class="'border-2 border-solid'"
+                    :style="'border-color: ' + tag.color">
+                  </div>
+
+                  <span class="text-lg" :class="{'text-primary': mailFilter == tag.value}">{{ tag.text }}</span>
+                </router-link>
+
+              </div>
             </div>
         </VuePerfectScrollbar>
     </div>
