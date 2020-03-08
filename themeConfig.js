b523aa3697a7419b33de5cdc088ccee9c8ea0e94
Stange Uwe <stange@hdm-stuttgart.de>
2020-03-08 17:59:04 +0100

    UI: keep footer visible at questionnaire

--- themeConfig.js
+++ themeConfig.js
@@ -29,7 +29,7 @@ const themeConfig = {
   hideScrollToTop   : false,       // options[Boolean] : true, false(default)
   mainLayoutType    : "horizontal",  // options[String]  : vertical(default) / horizontal
   navbarColor       : "#fff",      // options[String]  : HEX color / rgb / rgba / Valid HTML Color name - (default: #fff)
-  navbarType        : "floating",  // options[String]  : floating(default) / static / sticky / hidden
+  navbarType        : "sticky",  // options[String]  : floating(default) / static / sticky / hidden
   routerTransition  : "zoom-fade", // options[String]  : zoom-fade / slide-fade / fade-bottom / fade / zoom-out / none(default)
   sidebarCollapsed  : true,       // options[Boolean] : true, false(default)
   theme             : "light",     // options[String]  : "light"(default), "dark", "semi-dark"
