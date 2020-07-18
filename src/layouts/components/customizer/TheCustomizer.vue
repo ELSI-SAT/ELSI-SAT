ef4eab80791d33e66b5bc2583baa06049d7dafd8
Uwe Stange <uwe.stange@gmail.com>
2020-07-18 16:08:33 +0200

    Minor style tweaks: reduce box-shadows

--- src/layouts/components/customizer/TheCustomizer.vue
+++ src/layouts/components/customizer/TheCustomizer.vue
@@ -327,7 +327,7 @@ export default {
     width: 400px;
     max-width: 90vw;
     // @apply shadow-lg;
-    box-shadow: 0 15px 30px 0 rgba(0,0,0,0.11), 0 5px 15px 0 rgba(0,0,0,0.08);
+    box-shadow: none;
   }
 }
 
