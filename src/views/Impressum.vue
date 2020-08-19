d0367d035bd784f8b2942d7e05e3c9e4b4c5dfa4
Uwe Stange <uwe.stange@gmail.com>
2020-08-20 01:19:14 +0200

    Imprint

--- src/views/Impressum.vue
+++ src/views/Impressum.vue
@@ -1,5 +1,48 @@
 <template>
+  <div>
+    <div class="vx-row">
+      <div class="vx-col mb-base">
+        <vx-card
+          class="p-3"
+          title="Impressum"
+          title-color="primary"
+          subtitle="">
 
+          <p class="mb-1">Die Software „ELSI-SAT“ ist Produkt des BMBF-Forschungsprojektes „ELSI-SAT“, das an der Hochschule der Medien Stuttgart durchgeführt wurde.</p>
+          <p class="mb-1">Die Hochschule der Medien Stuttgart ist eine Körperschaft des öffentlichen Rechts. Sie wird durch den Rektor Professor Dr. Alexander W. Roos gesetzlich vertreten.</p>
+          <p class="mb-1">USt.-ID-Nr.: DE 224 427 890</p>
+
+          <h5 class="mt-5 mb-4">Verantwortlich für die Software ELSI-SAT</h5>
+
+          <p>Hochschule der Medien Stuttgart</p>
+          <p>Institut für digitale Ethik (IDE)</p>
+          <p>Prof. Dr. Tobias O. Keber</p>
+          <p>Nobelstr. 10</p>
+          <p>70569 Stuttgart</p>
+          <br>
+          <p>Mail: ide@hdm-stuttgart.de</p>
+          <p>Tel: 0711 8923 – 2299</p>
+          <br>
+
+          <h5 class="mt-5 mb-4">Zuständige Aufsichtsbehörde der Hochschule der Medien Stuttgart</h5>
+
+          <p>Ministerium für Wissenschaft, Forschung und Kunst Baden-Württemberg</p>
+          <p>Königstraße 46</p>
+          <p>70173 Stuttgart</p>
+
+          <h5 class="mt-5 mb-4">Haftungsausschluss</h5>
+
+          <h6 class="mt-5 mb-4">1. Inhalt des Onlineangebotes:</h6>
+          <p class="mb-1">Der Autor (Anbieter dieser Website) bzw. die einzelnen Verfasser übernimmt keinerlei Gewähr für die Aktualität, Korrektheit, Vollständigkeit oder Qualität der bereitgestellten Informationen. Haftungsansprüche gegen den Autor oder Verfasser, welche sich auf Schäden materieller oder ideeller Art beziehen, die durch die Nutzung oder Nichtnutzung der dargebotenen Informationen bzw. durch die Nutzung fehlerhafter und unvollständiger Informationen verursacht wurden, sind grundsätzlich ausgeschlossen, sofern seitens des Autors oder der Verfasser kein nachweislich vorsätzliches oder grob fahrlässiges Verschulden vorliegt.</p>
+          <p class="mb-1">Alle Angebote sind freibleibend und unverbindlich. Der Autor und die Verfasser behalten es sich ausdrücklich vor, Teile der Seiten oder das gesamte Angebot ohne gesonderte Ankündigung zu verändern, zu ergänzen, zu löschen oder die Veröffentlichung zeitweise oder endgültig einzustellen.</p>
+
+          <h6 class="mt-5 mb-4">2. Verweise und Links</h6>
+          <p class="mb-1">Bei direkten oder indirekten Verweisen auf fremde Internetseiten ("Links"), die außerhalb des Verantwortungsbereiches des Autors liegen, würde eine Haftungsverpflichtung ausschließlich in dem Fall in Kraft treten, in dem der Autor von den Inhalten Kenntnis hat und es ihm technisch möglich und zumutbar wäre, die Nutzung im Falle rechtswidriger Inhalte zu verhindern.</p>
+          <p class="mb-1">Der Autor erklärt hiermit ausdrücklich, dass zum Zeitpunkt der Linksetzung keine illegalen Inhalte auf den zu verlinkenden Seiten erkennbar waren. Auf die aktuelle und zukünftige Gestaltung, die Inhalte oder die Urheberschaft der gelinkten/verknüpften Seiten hat der Autor keinerlei Einfluss. Deshalb distanziert er sich hiermit ausdrücklich von allen Inhalten aller verlinkten/verknüpften Seiten, die nach der Linksetzung verändert wurden. Diese Feststellung gilt für alle innerhalb des eigenen Internetangebotes gesetzten Links und Verweise sowie für Fremdeinträge in vom Autor eingerichteten Gästebüchern, Diskussionsforen und Mailinglisten. Für illegale, fehlerhafte oder unvollständige Inhalte und insbesondere für Schäden, die aus der Nutzung oder Nichtnutzung solcherart dargebotener Informationen entstehen, haftet allein der Anbieter der Seite, auf welche verwiesen wurde, nicht derjenige, der über Links auf die jeweilige Veröffentlichung lediglich verweist.</p>
+        </vx-card>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
