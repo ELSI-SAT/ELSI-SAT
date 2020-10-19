8a331eb98fcf70a63d57eb3b6b8330435a092cde
Simon Wiest <wiest@hdm-stuttgart.de>
2020-10-19 11:13:31 +0200

    Add disclaimer pages legal/gender (cont.)

---
+++ src/views/Rechtliches.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="vx-row">
+      <div class="vx-col mb-base">
+        <vx-card
+          class="p-3"
+          title="Rechtlicher Hinweis"
+          title-color="black"
+          subtitle="">
+          <p class="mb-3">Das Ergebnis des Tools stellt eine unverbindiche Orientierungshilfe und keine verbindiche Rechts- oder Ethikberatung dar. Die Empfehlungen basieren auf den im Fragenkatalog ausgewählten Multiple-Choice-Antworten. Text- und Kommentarfelder werden in der aktuellen Version inhaltlich nicht ausgewertet.</p>
+        </vx-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Rechtlicher Hinweis"
+  }
+</script>
+
+<style scoped>
+
+</style>
