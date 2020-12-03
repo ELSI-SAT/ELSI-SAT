6a639df100275a95cf776676a63e3cb1bdd2e98c
List Tobias <listt@hdm-stuttgart.de>
2020-12-03 11:41:22 +0100

    Update Gender.vue

--- src/views/Gender.vue
+++ src/views/Gender.vue
@@ -6,8 +6,19 @@
           class="p-3"
           title="Gender-Hinweis"
           title-color="black"
-          subtitle="">
+          subtitle="Gender-Hinweis">
           <p class="mb-3">Der Fragenkatalog ist weitgehend genderneutral gehalten. In Einzelfällen sind Personenbezeichnungen mithilfe des Gendersterns „*“ gekennzeichnet. Diese Form der Bezeichnung inkludiert alle Geschlechter (männliche, weibliche, diverse Personen) gleichermaßen.</p>
+        </vx-card>
+      </div>
+    </div>
+    <div class="vx-row">
+      <div class="vx-col mb-base">
+        <vx-card
+          class="p-3"
+          title="technischer Hinweis"
+          title-color="black"
+          subtitle="">
+          <p class="mb-3">Die Ergebnisse und Scores basieren auf den im Fragenkatalog ausgewählten Multiple-Choice-Antworten. Text- und Kommentarfelder werden in der aktuellen Version nicht ausgewertet.</p>
         </vx-card>
       </div>
     </div>
@@ -16,7 +27,7 @@
 
 <script>
   export default {
-    name: "Gender-Hinweis"
+    name: "sonstige Hinweise"
   }
 </script>
 
