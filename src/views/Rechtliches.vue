d0e093a25664352ef64737b7be7ccaf70dd0f54f
List Tobias <listt@hdm-stuttgart.de>
2020-12-03 11:37:51 +0100

    Update Rechtliches.vue

--- src/views/Rechtliches.vue
+++ src/views/Rechtliches.vue
@@ -7,7 +7,7 @@
           title="Rechtlicher Hinweis"
           title-color="black"
           subtitle="">
-          <p class="mb-3">Das Ergebnis des Tools stellt eine unverbindiche Orientierungshilfe und keine verbindiche Rechts- oder Ethikberatung dar. Die Empfehlungen basieren auf den im Fragenkatalog ausgewählten Multiple-Choice-Antworten. Text- und Kommentarfelder werden in der aktuellen Version inhaltlich nicht ausgewertet.</p>
+          <p class="mb-3">Das Ergebnis des Tools stellt nur eine unverbindiche Orientierungshilfe und keine Rechtsberatung dar und kann diese auch nicht ersetzen.</p>
         </vx-card>
       </div>
     </div>
